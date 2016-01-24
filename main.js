99e5dcff2fa0ab519ad527499d8ea58640d5f12c
Tom Hanson <tomhanson1985@gmail.com>
2016-01-24 20:57:09 +0000

    added click event for space click

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ function Course(width, height, bombs) {
     this.height = height;
     this.totalSpace = this.width * this.height;
     this.bombs = bombs || (this.totalSpace / 100) * 33;
-
+    this.spaces = [];
 }
 //create a new space object for each space
 Course.prototype.createBoard = function() {
-    this.spaces = [];
     var x, y;
     y = 1;
     //run through each square and create a new space object which takes the coords as parameters
@@ -37,7 +36,7 @@ Course.prototype.addBombs = function() {
                 var diagDownLeft = (bombSpace + this.width) - 1;
                 var down = bombSpace + this.width;
                 var diagDownRight = (bombSpace + this.width) + 1;
-
+                //check each variable to test if it a valid item in the array
                 if (typeof this.spaces[diagUpLeft] !="undefined") {
                     this.spaces[diagUpLeft].bombsNear +=1;
                 }
@@ -65,7 +64,6 @@ Course.prototype.addBombs = function() {
 
             } else {
                 z++;
-                console.log('duplicate');
             }
             //this.armed.push([bombX, bombY]);
         }
@@ -73,59 +71,9 @@ Course.prototype.addBombs = function() {
             //create the space html
             this.spaces[cs].createSpace(cs);
         }
-        console.log(this.spaces);
     };
     this.bombSpaces();
 };
-//go through every space and check if its x & y coords match any of the ones in the armed coordinates array
-/*Course.prototype.armBombs = function() {
-    for(sp = 0; sp < this.spaces.length; sp++ ) {
-        //for each space loop through each item and check the armed array for any matches, set armed to true if it does match
-        for (bm = 0; bm < this.armed.length; bm++ ) {
-            if(this.spaces[sp].x === this.armed[bm][0] && this.spaces[sp].y === this.armed[bm][1]) {
-                this.spaces[sp].b = true;
-
-                this.spaces[sp-1].bombsNear += 1;
-                this.spaces[sp+1].bombsNear += 1;
-            }
-        }
-    }
-    for (cs = 0; cs < this.totalSpace; cs++ ) {
-        //create the space html
-        this.spaces[cs].createSpace(cs);
-    }
-    //check how many bombs are around the space
-    this.awareness(this.spaces);
-};
-//prototyype to figure out how many bombs are next to the space
-Course.prototype.awareness = function(param) {
-    for(a = 0; a < param.length; a++) {
-        if(param[a].b !== true ) {
-            //console.log('I am space ' + a + '. I am not armed, check my bomb awareness');
-            var diagUpLeft = (a - this.width ) - 1;
-            var up = a - this.width;
-            var diagUpRight = (a - this.width) + 1;
-            var back = a - 1;
-            var forward = a + 1;
-            var diagDownLeft = (a + this.width) - 1;
-            var down = a + this.width;
-            var diagDownRight = (a + this.width) + 1;
-
-
-
-
-
-            if( a - this.width >= 0 && a + this.width < this.totalSpace ) {
-
-                if(param[up].b == true ); {
-                    param[a].bombsNear += 1;
-                }
-            }
-        }
-    }
-    //console.log(this.spaces);
-};*/
-
 /*-------------------------------------------------------------------------------*/
 
 //create space object
@@ -135,33 +83,42 @@ function Space(x, y, b) {
     this.b = b;
     this.bombsNear = 0;
 }
-//self invoke this when using
 Space.prototype.createSpace = function(number) {
     //create divs to hold info
-    var sp = document.createElement("div");
-    var ol = document.createElement("div");
+    this.sp = document.createElement("div");
+    this.sp.classList.add('space');
+    this.sp.classList.add(this.b);
+    this.ol = document.createElement("div");
+    this.ol.classList.add('cover');
     //reference the container
-    var box = document.getElementById("minefield");
+    this.box = document.getElementById("minefield");
     //variables for how many bombs are nearby
     if(this.b !== true) {
-        var a = document.createTextNode(this.bombsNear);
+        this.a = document.createTextNode(this.bombsNear);
     } else {
-        var a = document.createTextNode("Bomb!");
+        this.a = document.createTextNode("Bomb!");
     }
-    //add classes to the divs
-    ol.classList.add('cover');
-    sp.classList.add('space');
-    sp.classList.add(this.b);
-    sp.appendChild(a);
-    sp.appendChild(ol);
-    box.appendChild(sp);
-};
+    //stitch it all together
+    this.sp.appendChild(this.a);
+    this.sp.appendChild(this.ol);
+    this.box.appendChild(this.sp);
 
+};
+Space.prototype.onClick = function() {
+    console.log(this.sp);
+    this.addEventListener = function() {
+        console.log(this.sp);
+    };
+    //this.addEventListener();
+    document.getElementById('minefield').onclick = function() {
+        console.log('clicked');
+    };
+}();
 
 x = new Course(10, 10, 30);
 x.createBoard();
 x.addBombs();
-//x.armBombs();
+
 
 
 
